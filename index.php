64586b5afd66753c2f7ecfe3d25a2b41d3719ec6
zeggy557 <zeggy@LAPTOP-74SOHQ25.localdomain>
2019-11-20 20:15:19 +0300

    My first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8" /> 
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
+		<script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+		<script type="text/javascript" src="fantasycalculator.js"></script>
+		<link rel="stylesheet" type="text/css" href="fantasycalculator.css">
+		<title>Zola Yapım</title>
+	</head>
+	<body>
