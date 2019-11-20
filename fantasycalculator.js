72c770910cb2b7fcb9c0741223d938aaeec51afe
zeggy557 <izgecan.gunal@gmail.com>
2019-11-20 23:11:52 +0300

    total wins check

--- fantasycalculator.js
+++ fantasycalculator.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
 	var teamsjson = $.getJSON( "fantasycalculator.json", function (data) {
-		  console.log(data);
 		  console.log(data.teams.donkeys.fullname);
+		  $.each(data.teams, function (key, val) {
+		  	var totalwins = 0;
+		  	$.each(val.winlist, function (key, val) {
+		  		totalwins += val;
+		  	});
+		  	var newdiv = "<div>" +  val.fullname + " W: " + val.wins + " TW: "+ totalwins + " L: " + val.losses + " DW: " + val.divwins + " DL: " + val.divlosses + "/<div>";
+		  	console.log(newdiv);
+		  	$(newdiv).appendTo("body"); 
+		  });
 
 	});
-	/*teams = JSON.Data;
-	console.log(teams);
-	console.log(teamsjson);
-	console.log("1");
-	console.log(teamsjson);
-	console.log("2");
-	console.log(teams);
-	$("body").append(teams[0]); */
+
 });
